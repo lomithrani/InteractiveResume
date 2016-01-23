e9b1dec4d6ddc42b1f1f451cba6eadcbc30a0092
Lomithrani <louisgentil89@gmail.com>
2016-01-23 10:56:46 +0100

    Added first controller and test RVR-3 #comment need to check if travis-ci runs tests #time 0d 2h

---
+++ InteractiveResume/api/controllers/ResumeController.js
@@ -0,0 +1,15 @@
+/**
+ * ResumeController
+ *
+ * @description :: Server-side logic for managing resumes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+
+
+module.exports = {
+    hi: function (req, res) {
+        return res.send("Hi there!");
+    }
+};
+
---
+++ InteractiveResume/test/unit/controllers/ResumeController.test.js
@@ -0,0 +1,15 @@
+﻿var assert = require('assert');
+
+var request = require('supertest');
+
+describe('ResumeController', function () {
+      
+        it('should say hi', function (done) {
+            request(sails.hooks.http.app)
+                .get('resume/hi')
+                .expect(200);
+
+        });
+  
+
+});
